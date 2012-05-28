89af9a27ab5d90823997dc26756031bcbac262b0
diegoramalhoufjf@gmail.com <diegoramalhoufjf@gmail.com@c5f59292-443b-1b83-4e8c-c650e4e4a56b>
2012-05-28 16:46:48 +0000

    Mips.h

---
+++ mips-compiler/mips/mips.h
@@ -0,0 +1,18 @@
+#ifndef MIPS_H_INCLUDED
+#define MIPS_H_INCLUDED
+
+#define maxInstrucoes 64
+
+
+/***************************************************************************************/
+/********************************* Variaveis Globais ***********************************/
+/***************************************************************************************/
+
+unsigned int mem[maxInstrucoes];
+int PC;
+int reg[32];      //Registradores
+
+
+void executaInstrucoes(int qntInstrucoes);
+
+#endif // MIPS_H_INCLUDED
