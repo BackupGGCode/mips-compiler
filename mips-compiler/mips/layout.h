892f1c62b6d5ee9cd7a85b6a89c25f6a82f0e948
diegoramalhoufjf@gmail.com <diegoramalhoufjf@gmail.com@c5f59292-443b-1b83-4e8c-c650e4e4a56b>
2012-05-28 16:45:34 +0000

    Lib Manipulador do layout

---
+++ mips-compiler/mips/layout.h
@@ -0,0 +1,21 @@
+#ifndef LAYOUT_H_INCLUDED
+#define LAYOUT_H_INCLUDED
+
+/**
+** Procedimento para pausar um tempo(segundos float)
+**/
+void wait ( float segundos );
+
+/**
+** Procedimento para limpar o prompt
+**/
+void limpaPrompt();
+
+/**
+** Função que mostra o menu
+** Retorna(int)
+   opção selecionada
+**/
+int selecionaMenu();
+
+#endif // LAYOUT_H_INCLUDED
